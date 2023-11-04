11ac74719c14a4556c9cbf89904d964c72689d8c
Hirak Bhattacharjee <bh.hirak@gmail.com>
2023-11-04 22:46:32 +0530

    BaseSubscriber

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -2,20 +2,42 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 
+import org.reactivestreams.Subscription;
+
+import reactor.core.publisher.BaseSubscriber;
+
 public class Exercise5 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
-        // Subscribe to a flux using the error and completion hooks
+        // Subscribe to a mono using the error and completion hooks
         // TODO: Write code here
+    	//ReactiveSources.intNumberMono().subscribe(i->System.out.println(i), t -> t.printStackTrace());
 
-        // Subscribe to a flux using an implementation of BaseSubscriber
+        // Subscribe to a mono using an implementation of BaseSubscriber
         // TODO: Write code here
+    	ReactiveSources.intNumbersFlux().subscribe(new MySubscriber<>());
 
         System.out.println("Press a key to end");
         System.in.read();
     }
 
+}
+
+class MySubscriber<T> extends BaseSubscriber<T>{
+	@Override
+	protected void hookOnSubscribe(Subscription subscription) {
+		// TODO Auto-generated method stub
+		System.out.println("Subscription happened");
+		super.hookOnSubscribe(subscription);
+	}
+	
+	@Override
+	protected void hookOnNext(T value) {
+		// TODO Auto-generated method stub
+		System.out.println(value.toString()+" received");
+		super.hookOnNext(value);
+	}
 }
